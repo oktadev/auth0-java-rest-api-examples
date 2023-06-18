351b1fcabfd4f43d393daeddc1bc8115dbce1ef4
Matt Raible <matt@raibledesigns.com>
2023-06-18 10:38:46 -0600

    Simplify Helidon controller

--- helidon/src/main/java/com/okta/rest/controller/HelloResource.java
+++ helidon/src/main/java/com/okta/rest/controller/HelloResource.java
@@ -1,24 +1,18 @@
 package com.okta.rest.controller;
 
-import io.helidon.security.Principal;
 import io.helidon.security.SecurityContext;
+import io.helidon.security.annotations.Authenticated;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
-import java.util.Optional;
-
-import static jakarta.ws.rs.core.MediaType.TEXT_PLAIN;
 
 @Path("/hello")
 public class HelloResource {
 
+    @Authenticated
     @GET
-    @Path("/")
-    @Produces(TEXT_PLAIN)
     public String hello(@Context SecurityContext context) {
-        Optional<Principal> userPrincipal = context.userPrincipal();
-        return "Hello, " + userPrincipal.get().getName() + "!";
+        return "Hello, " + context.userName() + "!";
     }
 }
