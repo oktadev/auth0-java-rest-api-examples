4cb8034d3accc1488eb77b72090c0daf7d1cfadc
Matt Raible <matt@raibledesigns.com>
2023-06-20 00:37:11 -0600

    Add JDK switching and logging

--- build.sh
+++ build.sh
@@ -6,10 +6,18 @@ set -e
 rm -rf {micronaut,quarkus,spring-boot}/build
 rm -rf helidon/target
 
+source ~/.sdkman/bin/sdkman-init.sh
+sdk use java 22.3.1.r19-grl
+
+echo "Building Micronaut..."
 cd micronaut && ./gradlew nativeCompile
 
+echo "Building Quarkus..."
 cd ../quarkus && ./gradlew build -Dquarkus.package.type=native
 
+echo "Building Spring Boot..."
 cd ../spring-boot && ./gradlew nativeCompile
 
+echo "Building Helidon..."
+sdk use java 22.3.1.r17-grl
 cd ../helidon && mvn package -Pnative-image
