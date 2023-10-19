4e2b1073f27147afc67f19d589e260ac912566e9
Matt Raible <matt@raibledesigns.com>
2023-10-19 16:32:37 -0400

    Print warning if fkill-cli is missing

--- start.sh
+++ start.sh
@@ -39,6 +39,7 @@ kill_cmd="fuser -k -n tcp 8080"
 if [[ "$OSTYPE" == "darwin"* ]]; then
    gdate --version || echo "gdate is missing. It is used to record timestamps on macOS. Install it using 'brew install coreutils'."
    date_cmd="gdate"
+   fkill --version || echo "fkill is missing. Install it using 'npm i -g fkill-cli'."
    kill_cmd="fkill :8080"
 fi
 
