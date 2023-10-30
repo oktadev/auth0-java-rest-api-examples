89df55ea2546189a902f08ff08e249a3dd1514c9
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-10-30 09:30:47 -0400

    more factory like

--- helidon-se/src/main/java/com/okta/rest/config/SecurityConfig.java
+++ helidon-se/src/main/java/com/okta/rest/config/SecurityConfig.java
@@ -1,20 +1,25 @@
 package com.okta.rest.config;
 
 import io.avaje.config.Config;
+import io.avaje.inject.Bean;
 import io.avaje.inject.Factory;
 import io.helidon.common.configurable.Resource;
 import io.helidon.security.Security;
 import io.helidon.security.providers.jwt.JwtProvider;
-import io.helidon.webserver.WebServerConfig;
 import io.helidon.webserver.context.ContextFeature;
 import io.helidon.webserver.security.SecurityFeature;
-import jakarta.inject.Inject;
+import io.helidon.webserver.spi.ServerFeature;
 
 @Factory
 public class SecurityConfig {
 
-  @Inject
-  void ctx(WebServerConfig.Builder builder) {
+  @Bean
+  ServerFeature ctx() {
+    return ContextFeature.create();
+  }
+
+  @Bean
+  ServerFeature oauth() {
 
     var oauth =
         JwtProvider.builder()
@@ -22,12 +27,10 @@ public class SecurityConfig {
             .verifyJwk(Resource.create(Config.getURI("se.jwt.verify.publickey.location")))
             .build();
     Security security = Security.builder().addProvider(oauth).build();
-    var securityFeature =
-        SecurityFeature.create(
-            sfb ->
-                sfb.security(security)
-                    .addPath(p -> p.path("/hello").handler(h -> h.authenticate(true))));
-    builder.addFeature(securityFeature);
-    builder.addFeature(ContextFeature.create());
+
+    return SecurityFeature.create(
+        sfb ->
+            sfb.security(security)
+                .addPath(p -> p.path("/hello").handler(h -> h.authenticate(true))));
   }
 }
